0691d019ca224eecdbf0eb7a1e883dc05d478198
Clément Godbarge <cag437@nyu.edu>
2024-04-10 11:28:24 +0100

    Update multi-saxon.py
    Major update: bug fixes, improved error handling, creation of a Toml config file.

--- multi-saxon.py
+++ multi-saxon.py
@@ -5,6 +5,7 @@ import threading
 import logging
 import multiprocessing
 import xml.etree.ElementTree as ET
+import toml
 
 from tqdm import tqdm
 from multiprocessing import Manager
@@ -12,18 +13,14 @@ from tqdm.contrib.concurrent import process_map
 
 from saxonche import PySaxonProcessor, PyXslt30Processor
 
-logging.basicConfig(filename='errors.log', level=logging.INFO)
+# Load configuration from TOML file
+config = toml.load("config.toml")
 
-# global variables
-root_dir = '/Users/clem/Documents/EEBO/'
-output_dir = '/Users/clem/Desktop/EEBOTest4'
-metadata_dir = '/Users/clem/Desktop/EEBOTest4/metadata'
-xslt_file_path = '/Users/clem/Desktop/EEBOTest4/scripts/transformEEBO.xsl'
-os.makedirs(metadata_dir, exist_ok=True)
-csv_file_path = os.path.join(metadata_dir, 'metadata.csv')
+logging.basicConfig(filename=config["logging"]["filename"], level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-# utility functions
-def get_metadata(xml_tree):
+def get_metadata(xml_tree: ET.ElementTree) -> list:
+    """Extract metadata from the XML tree."""
     title_el = xml_tree.find('.//tei:titleStmt/tei:title', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
     author_el = xml_tree.find('.//tei:titleStmt/tei:author', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
     date_el = xml_tree.find('.//tei:sourceDesc/tei:biblFull/tei:publicationStmt/tei:date', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
@@ -35,20 +32,25 @@ def get_metadata(xml_tree):
     return [title, author, date, language]
 
 
-def count_words_in_file(file_path):
+def count_words_in_file(file_path: str) -> int:
+    """Count the number of words in a file."""
     with open(file_path, 'r') as file:
         text = file.read()
     return len(text.split())
 
 
 def heartbeat():
+    """Print a dot every 10 seconds to indicate the script is running."""
     while True:
         print(".", end="", flush=True)
-        time.sleep(10)  # print a dot every 10 seconds
+        time.sleep(10)
+
 
 threading.Thread(target=heartbeat, daemon=True).start()
 
-def process_files(file_chunk, xslt_content, progress_value):
+
+def process_files(file_chunk: list, xslt_content: str, progress_value: multiprocessing.Value) -> list:
+    """Process a chunk of XML files, transform them, extract metadata, and count words."""
     results = []
 
     # Initialize the Saxon processor for each worker
@@ -56,12 +58,11 @@ def process_files(file_chunk, xslt_content, progress_value):
         xslt_processor = proc.new_xslt30_processor()
         compiled_stylesheet = xslt_processor.compile_stylesheet(stylesheet_text=xslt_content)
 
-        for filename in file_chunk:
+        for xml_file_path in file_chunk:
             try:
-                #print(f"Processing: {filename}") # debug
-                xml_file_path = filename
-                output_file_path = os.path.join(output_dir, os.path.basename(filename).replace('.xml', '.txt'))
-                # Transform the XML file using the compiled
+                output_file_path = os.path.join(config["output"]["directory"], os.path.basename(xml_file_path).replace('.xml', '.txt'))
+                
+                # Transform the XML file using the compiled stylesheet
                 compiled_stylesheet.transform_to_file(source_file=xml_file_path, output_file=output_file_path)
 
                 # Parse XML and get metadata
@@ -69,37 +70,44 @@ def process_files(file_chunk, xslt_content, progress_value):
                 metadata = get_metadata(xml_tree)
 
                 # Create language-specific output directory
-                language = metadata[3] if metadata[3] else "Unknown"
-                language_dir = os.path.join(output_dir, language)
+                language = metadata[3]
+                language_dir = os.path.join(config["output"]["directory"], language)
                 os.makedirs(language_dir, exist_ok=True)
 
-                output_file_path_lang = os.path.join(language_dir, os.path.basename(filename).replace('.xml', '.txt'))
+                output_file_path_lang = os.path.join(language_dir, os.path.basename(xml_file_path).replace('.xml', '.txt'))
                 os.rename(output_file_path, output_file_path_lang)
+                
                 # Count words in TXT file
                 word_count = count_words_in_file(output_file_path_lang)
                 results.append(metadata + [word_count])
                 progress_value.value += 1
 
-                # print(f"Updated Progress Value: {progress.value}") # debug
-            except Exception as e:
-                print(f"Error processing {filename}: {e}")
-                logging.error(f"Error processing {filename}: {e}")
+            except ET.ParseError as parse_error:
+                logging.error(f"Error parsing XML file {xml_file_path}: {parse_error}")
+            except IOError as io_error:
+                logging.error(f"Error reading or writing file {xml_file_path}: {io_error}")
+            except Exception as exception:
+                logging.error(f"Error processing file {xml_file_path}: {exception}")
 
     return results
 
-def wrapper(args):
+
+def wrapper(args: tuple) -> list:
+    """Wrapper function for process_files to unpack arguments."""
     return process_files(*args)
+
 
 if __name__ == '__main__':
     try:
-        all_files = [os.path.join(dirpath, filename) for dirpath, dirnames, filenames in os.walk(root_dir) for filename in filenames if filename.endswith('.xml')]
+        all_files = [os.path.join(dirpath, filename) for dirpath, dirnames, filenames in os.walk(config["input"]["directory"]) for filename in filenames if filename.endswith('.xml')]
         num_processes = multiprocessing.cpu_count()
+        
         with Manager() as manager:
             progress_value = manager.Value('i', 0)
             done_event = manager.Event()
             pbar = tqdm(total=len(all_files))
 
-            def update_progress_bar(progress_value):
+            def update_progress_bar(progress_value: multiprocessing.Value):
                 while not done_event.is_set():
                     pbar.update(progress_value.value - pbar.n)
                     time.sleep(0.5)
@@ -109,7 +117,7 @@ if __name__ == '__main__':
 
             chunks = [all_files[i::num_processes] for i in range(num_processes)]
 
-            with open(xslt_file_path, 'r') as file:
+            with open(config["xslt"]["file_path"], 'r') as file:
                 xslt_content = file.read()
 
             pool = multiprocessing.Pool(processes=num_processes)
@@ -119,7 +127,7 @@ if __name__ == '__main__':
             done_event.set()
             progress_thread.join()
 
-        with open(csv_file_path, 'w', newline='') as file:
+        with open(config["output"]["metadata_file"], 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Title", "Author", "Date", "Language", "Word Count"])
             for result_chunk in results:
@@ -127,11 +135,10 @@ if __name__ == '__main__':
                     writer.writerow(row)
 
     except KeyboardInterrupt:
-        print("\\nReceived keyboard interrupt.")
+        logging.info("Received keyboard interrupt. Exiting.")
         if 'pool' in locals():
-           pool.terminate()
-           pool.join()
-        print("Exiting now...")
+            pool.terminate()
+            pool.join()
 
     finally:
-        print("Peace.")
+        logging.info("Script execution completed.")
